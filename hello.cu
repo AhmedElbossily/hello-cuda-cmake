f6b3b60bf66e37fe5f7295d0ccabf4543587571f
AhmedElbossily <ahmed.fathy.elbossily@gmail.com>
2023-10-20 15:30:14 +0300

    adding outputfile

--- hello.cu
+++ hello.cu
@@ -2,6 +2,9 @@
 __global__ void myKernel(void) {
 }
 int main(void) {
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+	
 	myKernel <<<1, 1>>>();
 	printf("Hello CUDA!\n");
 	return 0;
